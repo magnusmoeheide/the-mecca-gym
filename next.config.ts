75ceef744c15fa5fdce690ccc726d55c2f5bc945
magnusmoeheide <magnus.heide@code.berlin>
2025-04-13 14:50:43 +0200

    github

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  distDir: "out",
   images: {
+    unoptimized: true,
     domains: ["hebbkx1anhila5yf.public.blob.vercel-storage.com"],
   },
+  basePath: "/the-mecca-gym", // <-- use your repo name here
 };
 
 export default nextConfig;
