1bd3559339b7e1309cec73ebd7633901e1ecf587
magnusmoeheide <magnus.heide@code.berlin>
2025-04-13 15:00:44 +0200

    latest

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  distDir: "out",
   images: {
-    unoptimized: true,
     domains: ["hebbkx1anhila5yf.public.blob.vercel-storage.com"],
   },
 };
